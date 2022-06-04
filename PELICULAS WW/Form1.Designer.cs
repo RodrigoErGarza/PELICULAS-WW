cad58b3ba9d6f794651a5dc997aa43ec97c900ca
alumno <alumno@UMG-LAB>
2022-06-03 22:19:08 -0600

    Agregar archivos de proyecto.

---
+++ PELICULAS WW/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace PELICULAS_WW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonAñadir = new System.Windows.Forms.Button();
+            this.buttonActualizar = new System.Windows.Forms.Button();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            this.textBoxEstreno = new System.Windows.Forms.TextBox();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.textBoxDirector = new System.Windows.Forms.TextBox();
+            this.textBoxTaquilla = new System.Windows.Forms.TextBox();
+            this.textBoxGenero = new System.Windows.Forms.TextBox();
+            this.textBoxProtagonistas = new System.Windows.Forms.TextBox();
+            this.textBoxCompañia = new System.Windows.Forms.TextBox();
+            this.GridWizzardWorl = new System.Windows.Forms.DataGridView();
+            this.textBoxIdpeliculas = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.GridWizzardWorl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(353, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(626, 62);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WIZARDING WORLD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Titulo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(66, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Director";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(66, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Taquilla";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(66, 359);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 27);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Genero";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(66, 403);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(179, 27);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Protagonistas";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(66, 446);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 27);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Compañia";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(66, 173);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 27);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Estreno";
+            // 
+            // buttonAñadir
+            // 
+            this.buttonAñadir.BackColor = System.Drawing.Color.Wheat;
+            this.buttonAñadir.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAñadir.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonAñadir.Location = new System.Drawing.Point(161, 500);
+            this.buttonAñadir.Name = "buttonAñadir";
+            this.buttonAñadir.Size = new System.Drawing.Size(140, 50);
+            this.buttonAñadir.TabIndex = 9;
+            this.buttonAñadir.Text = "Añadir";
+            this.buttonAñadir.UseVisualStyleBackColor = false;
+            this.buttonAñadir.Click += new System.EventHandler(this.buttonAñadir_Click);
+            // 
+            // buttonActualizar
+            // 
+            this.buttonActualizar.BackColor = System.Drawing.Color.Wheat;
+            this.buttonActualizar.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonActualizar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonActualizar.Location = new System.Drawing.Point(325, 500);
+            this.buttonActualizar.Name = "buttonActualizar";
+            this.buttonActualizar.Size = new System.Drawing.Size(141, 50);
+            this.buttonActualizar.TabIndex = 10;
+            this.buttonActualizar.Text = "Actualizar";
+            this.buttonActualizar.UseVisualStyleBackColor = false;
+            this.buttonActualizar.Click += new System.EventHandler(this.buttonActualizar_Click);
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.BackColor = System.Drawing.Color.Wheat;
+            this.buttonEliminar.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEliminar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonEliminar.Location = new System.Drawing.Point(486, 500);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(151, 50);
+            this.buttonEliminar.TabIndex = 11;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = false;
+            this.buttonEliminar.Click += new System.EventHandler(this.buttonEliminar_Click);
+            // 
+            // textBoxEstreno
+            // 
+            this.textBoxEstreno.BackColor = System.Drawing.Color.Wheat;
+            this.textBoxEstreno.Location = new System.Drawing.Point(276, 173);
+            this.textBoxEstreno.Name = "textBoxEstreno";
+            this.textBoxEstreno.Size = new System.Drawing.Size(340, 20);
+            this.textBoxEstreno.TabIndex = 12;
+            this.textBoxEstreno.TextChanged += new System.EventHandler(this.textBoxEstreno_TextChanged);
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.BackColor = System.Drawing.Color.Wheat;
+            this.textBoxTitulo.Location = new System.Drawing.Point(276, 220);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(340, 20);
+            this.textBoxTitulo.TabIndex = 13;
+            // 
+            // textBoxDirector
+            // 
+            this.textBoxDirector.BackColor = System.Drawing.Color.Wheat;
+            this.textBoxDirector.Location = new System.Drawing.Point(276, 263);
+            this.textBoxDirector.Name = "textBoxDirector";
+            this.textBoxDirector.Size = new System.Drawing.Size(340, 20);
+            this.textBoxDirector.TabIndex = 14;
+            // 
+            // textBoxTaquilla
+            // 
+            this.textBoxTaquilla.BackColor = System.Drawing.Color.Wheat;
+            this.textBoxTaquilla.Location = new System.Drawing.Point(276, 311);
+            this.textBoxTaquilla.Name = "textBoxTaquilla";
+            this.textBoxTaquilla.Size = new System.Drawing.Size(340, 20);
+            this.textBoxTaquilla.TabIndex = 15;
+            // 
+            // textBoxGenero
+            // 
+            this.textBoxGenero.BackColor = System.Drawing.Color.Wheat;
+            this.textBoxGenero.Location = new System.Drawing.Point(276, 359);
+            this.textBoxGenero.Name = "textBoxGenero";
+            this.textBoxGenero.Size = new System.Drawing.Size(340, 20);
+            this.textBoxGenero.TabIndex = 16;
+            // 
+            // textBoxProtagonistas
+            // 
+            this.textBoxProtagonistas.BackColor = System.Drawing.Color.Wheat;
+            this.textBoxProtagonistas.Location = new System.Drawing.Point(276, 403);
+            this.textBoxProtagonistas.Name = "textBoxProtagonistas";
+            this.textBoxProtagonistas.Size = new System.Drawing.Size(340, 20);
+            this.textBoxProtagonistas.TabIndex = 17;
+            // 
+            // textBoxCompañia
+            // 
+            this.textBoxCompañia.BackColor = System.Drawing.Color.Wheat;
+            this.textBoxCompañia.Location = new System.Drawing.Point(276, 446);
+            this.textBoxCompañia.Name = "textBoxCompañia";
+            this.textBoxCompañia.Size = new System.Drawing.Size(340, 20);
+            this.textBoxCompañia.TabIndex = 18;
+            // 
+            // GridWizzardWorl
+            // 
+            this.GridWizzardWorl.BackgroundColor = System.Drawing.Color.Wheat;
+            this.GridWizzardWorl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridWizzardWorl.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.GridWizzardWorl.Location = new System.Drawing.Point(735, 112);
+            this.GridWizzardWorl.MultiSelect = false;
+            this.GridWizzardWorl.Name = "GridWizzardWorl";
+            this.GridWizzardWorl.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.GridWizzardWorl.Size = new System.Drawing.Size(395, 371);
+            this.GridWizzardWorl.TabIndex = 19;
+            this.GridWizzardWorl.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridWizzardWorl_CellContentClick);
+            this.GridWizzardWorl.Click += new System.EventHandler(this.GridWizzardWorl_Click);
+            // 
+            // textBoxIdpeliculas
+            // 
+            this.textBoxIdpeliculas.BackColor = System.Drawing.Color.Wheat;
+            this.textBoxIdpeliculas.Location = new System.Drawing.Point(276, 131);
+            this.textBoxIdpeliculas.Name = "textBoxIdpeliculas";
+            this.textBoxIdpeliculas.Size = new System.Drawing.Size(340, 20);
+            this.textBoxIdpeliculas.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(66, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(134, 27);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "IdPelicula";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PELICULAS_WW.Properties.Resources.FONDO_CARTA_HOWARTS;
+            this.ClientSize = new System.Drawing.Size(1214, 588);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxIdpeliculas);
+            this.Controls.Add(this.GridWizzardWorl);
+            this.Controls.Add(this.textBoxCompañia);
+            this.Controls.Add(this.textBoxProtagonistas);
+            this.Controls.Add(this.textBoxGenero);
+            this.Controls.Add(this.textBoxTaquilla);
+            this.Controls.Add(this.textBoxDirector);
+            this.Controls.Add(this.textBoxTitulo);
+            this.Controls.Add(this.textBoxEstreno);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.buttonActualizar);
+            this.Controls.Add(this.buttonAñadir);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridWizzardWorl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonAñadir;
+        private System.Windows.Forms.Button buttonActualizar;
+        private System.Windows.Forms.Button buttonEliminar;
+        private System.Windows.Forms.TextBox textBoxEstreno;
+        private System.Windows.Forms.TextBox textBoxTitulo;
+        private System.Windows.Forms.TextBox textBoxDirector;
+        private System.Windows.Forms.TextBox textBoxTaquilla;
+        private System.Windows.Forms.TextBox textBoxGenero;
+        private System.Windows.Forms.TextBox textBoxProtagonistas;
+        private System.Windows.Forms.TextBox textBoxCompañia;
+        private System.Windows.Forms.DataGridView GridWizzardWorl;
+        private System.Windows.Forms.TextBox textBoxIdpeliculas;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ PELICULAS WW/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PELICULAS_WW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gridcar();
+        }
+
+        private void gridcar()
+        {
+            PeliculasEntitiesConexion pelicontex = new PeliculasEntitiesConexion();
+            GridWizzardWorl.DataSource = pelicontex.tb_peliculas.ToList();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAñadir_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(this.textBoxIdpeliculas.Text);
+            string estre=textBoxEstreno.Text;
+            string titu=textBoxTitulo.Text;
+            string direc=textBoxDirector.Text;
+            int taq=int.Parse(this.textBoxTaquilla.Text);
+            string gen=textBoxGenero.Text;
+            string prota=textBoxProtagonistas.Text;
+            string compa=textBoxCompañia.Text;
+
+            using (PeliculasEntitiesConexion pelicontext= new PeliculasEntitiesConexion())
+            {
+                tb_peliculas pww = new tb_peliculas
+                {
+                    IdPelicula = id,
+                    Estreno = estre,
+                    Titulo = titu,
+                    Director = direc,
+                    Taquilla = taq,
+                    Genero = gen,
+                    Protagonistas = prota,
+                    Compañia = compa,
+                };
+                pelicontext.tb_peliculas.Add(pww);
+                pelicontext.SaveChanges();
+                gridcar();
+            }
+        }
+
+        private void obtedato()
+        {
+            this.textBoxIdpeliculas.Text = GridWizzardWorl.SelectedRows[0].Cells[0].Value.ToString();
+            this.textBoxEstreno.Text= GridWizzardWorl.SelectedRows[0].Cells[1].Value.ToString();
+            this.textBoxTitulo.Text= GridWizzardWorl.SelectedRows[0].Cells[2].Value.ToString();
+            this.textBoxDirector.Text= GridWizzardWorl.SelectedRows[0].Cells[3].Value.ToString();
+            this.textBoxTaquilla.Text= GridWizzardWorl.SelectedRows[0].Cells[4].Value.ToString();
+            this.textBoxGenero.Text= GridWizzardWorl.SelectedRows[0].Cells[5].Value.ToString();
+            this.textBoxProtagonistas.Text= GridWizzardWorl.SelectedRows[0].Cells[6].Value.ToString();
+            this.textBoxCompañia.Text= GridWizzardWorl.SelectedRows[0].Cells[7].Value.ToString();
+        }
+
+        private void GridWizzardWorl_Click(object sender, EventArgs e)
+        {
+            obtedato();
+        }
+
+        private void buttonActualizar_Click(object sender, EventArgs e)
+        {
+            int idp = Convert.ToInt16(this.textBoxIdpeliculas.Text);
+            string estre = textBoxEstreno.Text;
+            string titu = textBoxTitulo.Text;
+            string direc = textBoxDirector.Text;
+            int taq = int.Parse(this.textBoxTaquilla.Text);
+            string gen = textBoxGenero.Text;
+            string prota = textBoxProtagonistas.Text;
+            string compa = textBoxCompañia.Text;
+            using (PeliculasEntitiesConexion pelicontex= new PeliculasEntitiesConexion())
+            {
+                tb_peliculas pww = pelicontex.tb_peliculas.FirstOrDefault(consul => consul.IdPelicula == idp);
+                pww.Estreno = estre;
+                pww.Titulo = titu;
+                pww.Director = direc;
+                pww.Taquilla = taq;
+                pww.Genero=gen;
+                pww.Protagonistas=prota;
+                pww.Compañia = compa;
+                pelicontex.SaveChanges();
+                gridcar();
+            }
+        }
+
+        private void buttonEliminar_Click(object sender, EventArgs e)
+        {
+            int idp = Convert.ToInt16(this.textBoxIdpeliculas.Text);
+            using (PeliculasEntitiesConexion pelicontex = new PeliculasEntitiesConexion())
+            {
+                tb_peliculas pww = pelicontex.tb_peliculas.FirstOrDefault(consul => consul.IdPelicula == idp);
+                pelicontex.tb_peliculas.Remove(pww);
+                pelicontex.SaveChanges();
+                gridcar();
+            }
+        }
+
+        private void GridWizzardWorl_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBoxEstreno_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
